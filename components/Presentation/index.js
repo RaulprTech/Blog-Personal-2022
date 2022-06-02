512148d269275ec5f04ab509fd4641ceba8598e9
raulprtech <raul_vrm_2134@hotmail.com>
2022-06-02 18:49:14 -0500

    new image

--- components/Presentation/index.js
+++ components/Presentation/index.js
@@ -6,10 +6,20 @@ export default function Presentation() {
   const router = useRouter()
 
   return (
-    <section className="items-center px-2 py-8 md:px-0">
-      <div className="container mx-auto max-w-5xl items-center xl:px-5">
+    <section className="items-center justify-around px-2 py-8 md:px-0">
+      <div className="container mx-auto my-auto max-w-5xl items-center xl:px-5">
         <div className="flex flex-wrap items-center sm:-mx-3">
-          <div className="w-full md:w-1/2 md:px-3">
+          <div className="w-screen sm:mx-0 md:w-1/2 lg:mb-4">
+            <Image
+              src={`${router.basePath}/static/images/avatar_sf.png`}
+              alt="person with gray shirt looking in front"
+              width={360}
+              height={360}
+              layout={'fixed'}
+              className="rounded-3xl bg-transparent"
+            />
+          </div>
+          <div className="m-2 w-full sm:m-0 md:w-1/2 md:px-3">
             <div className="w-full space-y-6 pb-8 sm:max-w-md sm:pr-5 md:space-y-4 md:pb-0 lg:max-w-lg lg:space-y-8 lg:pr-0 xl:space-y-9">
               <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 dark:text-primary-400 sm:text-4xl md:text-3xl lg:text-4xl xl:text-5xl">
                 <span className="block text-2xl xl:inline">Hola, Soy </span>
@@ -20,7 +30,7 @@ export default function Presentation() {
               </h1>
               <div className="relative flex flex-col sm:flex-row sm:space-x-4">
                 <Link href="/projects">
-                  <a className="mb-3 flex w-full cursor-pointer items-center justify-center rounded-md bg-blue-400 px-6 py-2 text-lg text-white hover:bg-blue-500 sm:mb-0 sm:w-auto">
+                  <a className="mb-3 flex w-full cursor-pointer items-center justify-center rounded-md bg-primary-400 px-6 py-2 text-lg text-white hover:bg-primary-500 sm:mb-0 sm:w-auto">
                     Proyectos
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -44,15 +54,6 @@ export default function Presentation() {
                 </Link>
               </div>
             </div>
-          </div>
-          <div className="mx-0 w-screen md:w-1/2 lg:mb-4">
-            <Image
-              src={`${router.basePath}/static/images/dev-anime.png`}
-              alt="person writing in the computer"
-              width={360}
-              height={360}
-              layout={'fixed'}
-            />
           </div>
         </div>
       </div>
