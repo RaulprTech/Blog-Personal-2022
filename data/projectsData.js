62532925533cc72be639a5ac7f2c5b3e7cd1e0ea
raulprtech <raul_vrm_2134@hotmail.com>
2022-06-09 16:29:07 -0500

    fix routes

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,14 +2,14 @@ const projectsData = [
   {
     title: 'Google Clone',
     description: `Una replica del frontend del mas famoso motor de busquedas, hecha con HTML y CSS puros`,
-    imgSrc: '/static/images/google.png',
-    href: '/blog/google-clone',
+    imgSrc: '/static/images/blog/google.png',
+    href: '/blog/projects/google-clone',
   },
   {
     title: 'House for Sale',
     description: `Una atractiva landing page sobre un sitio donde poder llevar a cabo todo el proceso de venta de una casa`,
-    imgSrc: '/static/images/projects/house.png',
-    href: '/blog/houseforsale',
+    imgSrc: '/static/images/blog/house.png',
+    href: '/blog/projects/houseforsale',
   },
 ]
 
