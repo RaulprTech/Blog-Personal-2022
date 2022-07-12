cc25445fb7a00cacf4a4e3cae74f5b372c1fb6e0
raulprtech <raul_vrm_2134@hotmail.com>
2022-07-12 16:04:09 -0500

    admin bug fixed

--- pages/_document.js
+++ pages/_document.js
@@ -23,6 +23,7 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#000000" />
           <meta name="theme-color" content="#000000" />
           <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+          <script async src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         </Head>
         <body className="bg-white text-black antialiased dark:bg-gray-900 dark:text-white">
           <Main />
