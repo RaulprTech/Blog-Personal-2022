1aec28184b12c14bb4a17416639e4c8800d1467a
raulprtech <raul_vrm_2134@hotmail.com>
2022-11-22 02:29:13 -0600

    title h2 and h4 added

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -45,9 +45,9 @@ export default function AuthorLayout({ children, frontMatter }) {
               height="192"
               className="h-48 w-48 rounded-full"
             />
-            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
-            <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            <h2 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h2>
+            <h3 className="text-gray-500 dark:text-gray-400">{occupation}</h3>
+            <h4 className="text-gray-500 dark:text-gray-400">{company}</h4>
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
