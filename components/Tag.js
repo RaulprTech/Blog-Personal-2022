f68a56f6ba7f04fc4c55fdea0dee5e30be82e261
raulprtech <raul_vrm_2134@hotmail.com>
2022-11-24 01:41:16 -0600

    new Tag component

--- components/Tag.js
+++ components/Tag.js
@@ -1,14 +1,35 @@
 import Link from 'next/link'
 import kebabCase from '@/lib/utils/kebabCase'
+import Button from '@/components/Button'
 
-const Tag = ({ text }) => {
+const Tag = ({ text, children }) => {
   return (
-    <Link href={`/tags/${kebabCase(text)}`}>
-      <span className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-        {text.split(' ').join('-')}
-      </span>
+    <Link
+      className="group m-1 cursor-pointer text-center"
+      href={`/tags/${kebabCase(text)}`}
+      target="_blank"
+      rel="noreferrer"
+    >
+      {text && (
+        <div className="flex w-full content-center justify-between rounded-lg border border-primary-700 bg-white py-1 px-3 text-xs font-semibold text-primary-500 group-hover:bg-primary-500 group-hover:text-gray-100 dark:border-primary-500 dark:bg-gray-900 lg:text-sm">
+          <span className=" w-full">{text.split(' ').join('-')}</span>
+          {children && (
+            <span className="flex items-center space-x-5 pl-4 group-hover:text-white">
+              {children}
+            </span>
+          )}
+        </div>
+      )}
     </Link>
   )
 }
 
 export default Tag
+
+{
+  /* <Link href={`/tags/${kebabCase(text)}`}>
+<span className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+  {text.split(' ').join('-')}
+</span>
+</Link> */
+}
--- pages/tags.js
+++ pages/tags.js
@@ -21,24 +21,18 @@ export default function Tags({ tags }) {
           title={`Tags - ${siteMetadata.author}`}
           description={`Variedad de articulos sobre tecnologia escritos por ${siteMetadata.author}`}
         />
-        <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-8 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+        <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-6 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
           <div className="space-x-2 pb-6 md:space-y-5">
             <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-secondary-400 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
               Tags
             </h1>
           </div>
-          <div className="flex max-w-lg flex-wrap">
+          <div className="flex max-w-2xl flex-wrap">
             {Object.keys(tags).length === 0 && 'No tags found.'}
             {sortedTags.map((t) => {
               return (
-                <div key={t} className="mt-2 mb-2 mr-5">
-                  <Tag text={t} />
-                  <Link
-                    href={`/tags/${kebabCase(t)}`}
-                    className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
-                  >
-                    {` (${tags[t]})`}
-                  </Link>
+                <div key={t} className="m-1">
+                  <Tag text={t} link={`/tags/${kebabCase(t)}`}>{` (${tags[t]})`}</Tag>
                 </div>
               )
             })}
