fa50a332cfe66f5ba597c94de3fdf0d187163960
raulprtech <raul_vrm_2134@hotmail.com>
2022-11-23 22:37:20 -0600

    fixed color component

--- components/Quotes/quote.js
+++ components/Quotes/quote.js
@@ -15,13 +15,13 @@ export default function quote() {
             <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
           </svg>
           <Link href="https://www.instagram.com/p/CDrlWijjYTN/?utm_source=ig_web_copy_link">
-            <p className="relative text-neutral-700 dark:text-neutral-200">
-              Vive como si fueses a morir mañana. Aprende como si fueses a vivir para siempre
+            <p className="relative text-gray-900 dark:text-gray-200">
+              <b>Vive como si fueses a morir mañana. Aprende como si fueses a vivir por siempre</b>
             </p>
           </Link>
         </div>
         <footer className="mt-1">
-          <p className="text-sm text-neutral-500 dark:text-neutral-500">Mahatma Gandhi</p>
+          <p className="text-md text-gray-800 dark:text-gray-300">Mahatma Gandhi</p>
         </footer>
       </blockquote>
     </section>
