7b105d0b6499c2c4fde492b419b5acd1b4bcadf3
raulprtech <raul_vrm_2134@hotmail.com>
2022-11-23 22:41:03 -0600

    new data banners

---
+++ data/bannersData.js
@@ -0,0 +1,39 @@
+const bannersData = [
+  {
+    title: 'Conoce mi Blog!!',
+    link: 'http://raulpacheco.dev',
+    image: `/static/images/twitter-card.png`,
+    emoji: '🔗',
+    bgColor: 'blue',
+  },
+  {
+    title: '¿Qué se puede hacer con Javascript?',
+    link: 'https://raulpacheco.dev/blog/posts/que-se-puede-hacer-con-javascript',
+    image: `/static/images/blog/que-se-puede-hacer-con-javascript/portada.png`,
+    emoji: '📚',
+    bgColor: 'yellow',
+  },
+  {
+    title: '¿Por qué aprender React?',
+    link: 'https://raulpacheco.dev/blog/posts/porque-aprender-react-para-desarrollo-web',
+    image: `/static/images/blog/por-que-usar-react/portada.png`,
+    emoji: '⚛️',
+    bgColor: 'purple',
+  },
+  {
+    title: 'Mis Proyectos',
+    link: 'https://raulpacheco.dev/projects',
+    image: `/static/images/blog/que-se-puede-hacer-con-javascript/web.png`,
+    emoji: '🧑‍💻',
+    bgColor: 'green',
+  },
+  {
+    title: 'Descargar CV',
+    link: 'https://raulpacheco.dev/static/CV/CV.pdf',
+    image: `/static/images/cv.png`,
+    emoji: '📃',
+    bgColor: 'red',
+  },
+]
+
+module.exports = bannersData
---
+++ data/mainBanner.js
@@ -0,0 +1,9 @@
+const mainBanner = {
+  title: 'Bienvenido a mi Blog ✨',
+  link: 'http://raulpacheco.dev',
+  image: `/static/images/twitter-card.png`,
+  emoji: '🎉',
+  bgColor: 'blue',
+}
+
+module.exports = mainBanner
