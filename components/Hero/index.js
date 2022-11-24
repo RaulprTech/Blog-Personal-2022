3e434322e79f75d63c71bd4696763c06f5557f7f
raulprtech <raul_vrm_2134@hotmail.com>
2022-11-24 15:10:46 -0600

    buttons changed

--- components/Hero/index.js
+++ components/Hero/index.js
@@ -1,6 +1,7 @@
 import siteMetadata from '@/data/siteMetadata'
 // import SocialIcon from '@/components/social-icons'
 import IconCard from '../IconCard'
+import Button from '../Button'
 
 export default function Presentation() {
   // const router = useRouter()
@@ -102,14 +103,14 @@ export default function Presentation() {
         </div>
         <div className="flex items-end justify-center">
           <div className="grid grid-cols-2 grid-rows-1 gap-8 py-12 xl:grid-cols-1 xl:grid-rows-2">
-            <IconCard message="Creaciones" link="projects">
+            <Button text="Proyectos" link="projects">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="h-6 w-6 -rotate-6 dark:text-gray-100 dark:group-hover:text-green-400"
+                className="h-6 w-6 -rotate-6"
               >
                 <path
                   strokeLinecap="round"
@@ -117,15 +118,15 @@ export default function Presentation() {
                   d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
                 />
               </svg>
-            </IconCard>
-            <IconCard message="Mas de mi" link="about">
+            </Button>
+            <Button text="Mas de mi" link="about">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="h-6 w-6 -rotate-6 dark:text-gray-100 dark:group-hover:text-primary-400"
+                className="h-6 w-6 -rotate-6"
               >
                 <path
                   strokeLinecap="round"
@@ -133,7 +134,7 @@ export default function Presentation() {
                   d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
                 />
               </svg>
-            </IconCard>
+            </Button>
             {/* <IconCard message="Newsletter" link="projects">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
