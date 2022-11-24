983e3e365677d2d74901bf053742365c084584a5
raulprtech <raul_vrm_2134@hotmail.com>
2022-11-24 11:30:01 -0600

    added Capitalize

--- components/Tag.js
+++ components/Tag.js
@@ -6,12 +6,11 @@ const Tag = ({ text, children }) => {
     <Link
       className="group m-1 cursor-pointer text-center"
       href={`/tags/${kebabCase(text)}`}
-      target="_blank"
       rel="noreferrer"
     >
       {text && (
-        <div className="flex w-full content-center justify-between rounded-lg border border-primary-700 bg-white py-1 px-2 text-xs font-semibold text-primary-500 group-hover:bg-primary-500 group-hover:text-gray-100 dark:border-primary-500 dark:bg-gray-900 lg:text-sm">
-          <span className="w-full uppercase">{text.split(' ').join('-')}</span>
+        <div className="flex w-full content-center justify-between rounded-lg border border-primary-700 bg-white py-1 px-2 text-xs font-normal text-primary-500 group-hover:bg-primary-500 group-hover:text-gray-100 dark:border-primary-500 dark:bg-gray-900 lg:text-sm">
+          <span className="">{text.split(' ').join('-')}</span>
           {children && (
             <span className="flex items-center space-x-5 pl-4 group-hover:text-white">
               {children}
