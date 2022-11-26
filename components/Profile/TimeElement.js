8019d893564308a80ba4dc9e6f95110456e241d6
raulprtech <raul_vrm_2134@hotmail.com>
2022-11-25 21:23:12 -0600

    style element changed

--- components/Profile/TimeElement.js
+++ components/Profile/TimeElement.js
@@ -1,33 +1,19 @@
 import React from 'react'
 
-export default function TimeElement({
-  current = false,
-  jobTitle,
-  jobDescription,
-  date,
-  children,
-  place,
-}) {
+export default function TimeElement({ current = false, jobTitle, jobDescription, date, place }) {
   return (
-    <li className="mb-4 ml-8 rounded-md border border-gray-100 bg-gray-100  px-4  py-1 shadow-sm shadow-gray-300 dark:border-zinc-900 dark:bg-zinc-900 dark:shadow-none">
-      <span className=" absolute -left-3 flex h-6 w-6 animate-pulse items-center justify-center rounded-full bg-primary-200 ring-8 ring-gray-100 dark:bg-green-900 dark:ring-gray-900">
-        {children}
-      </span>
-      <h3 className="flex items-center text-base  font-semibold text-gray-900 dark:text-white">
-        {jobTitle}
-        {current && (
-          <span className="mr-2 ml-3 rounded bg-primary-100 px-2.5 py-0.5 text-sm font-medium text-primary-800 dark:bg-secondary-400 dark:text-gray-900">
-            Presente
-          </span>
-        )}
-      </h3>
-      <h4 className="flex items-center text-base font-normal text-primary-600 dark:text-primary-600">
-        {place}
-      </h4>
-      <time className="text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-        {date}
-      </time>
-      <p className="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">{jobDescription}</p>
+    <li>
+      <div className="flex-start flex items-center">
+        <div className="-ml-2 mr-3 -mt-2 flex h-4 w-4 items-center justify-center rounded-full bg-blue-600"></div>
+        <h4 className="-mt-2 text-xl font-semibold text-gray-100">{jobTitle}</h4>
+      </div>
+      <div className="ml-6">
+        <h5 className="text-lg font-semibold text-blue-600">{place}</h5>
+      </div>
+      <div className="ml-6 mb-6 pb-6">
+        <span className="text-sm">{date}</span>
+        <p className="mt-2 mb-4 text-gray-200">{jobDescription}</p>
+      </div>
     </li>
   )
 }
--- components/Profile/Timeline.js
+++ components/Profile/Timeline.js
@@ -1,19 +1,10 @@
-import { FaGraduationCap, FaBlackTie } from 'react-icons/fa'
-import { AiOutlineCode } from 'react-icons/ai'
-import { SiLeetcode } from 'react-icons/si'
-// import { Disclosure, Transition } from '@headlessui/react'
-import { HiChevronDown, HiOutlineDatabase } from 'react-icons/hi'
-import { FaBaby } from 'react-icons/fa'
-import { BsBuilding } from 'react-icons/bs'
-import { RiDoorClosedLine } from 'react-icons/ri'
-import { TbDeviceDesktopAnalytics } from 'react-icons/tb'
 import TimeElement from './TimeElement'
 
 export default function Timeline({ title, jobs }) {
   return (
     <div>
       <h1 className="text-center text-3xl font-bold text-gray-900 dark:text-gray-100">{title}</h1>
-      <ol className="relative mt-6 ml-6 border-l border-zinc-400 dark:border-gray-800 ">
+      <ol className="mt-8 border-l-2 border-blue-600">
         {jobs.map((job) => (
           <TimeElement
             current={job.current}
@@ -22,13 +13,7 @@ export default function Timeline({ title, jobs }) {
             date={job.date}
             jobDescription={job.description}
             key={job.title}
-          >
-            {job.type == 'professional' ? (
-              <FaBlackTie className="animate-none" />
-            ) : (
-              <FaGraduationCap />
-            )}
-          </TimeElement>
+          />
         ))}
       </ol>
     </div>
