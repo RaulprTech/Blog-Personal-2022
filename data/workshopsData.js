9372f29a8dd298625b678155ca6e07dc4eb7d77e
raulprtech <raul_vrm_2134@hotmail.com>
2022-11-25 21:25:09 -0600

    organization data

---
+++ data/workshopsData.js
@@ -0,0 +1,53 @@
+const workshopsData = [
+  {
+    title: 'Programación en Domotica',
+    place: 'CINVESTAV Unidad GDL',
+    type: 'professional',
+    date: 'Noviembre de 2022',
+    description:
+      'Taller sobre como construir proyectos de IoT y llevarlos de la idea a un producto',
+    current: false,
+  },
+  {
+    title: 'Programación de Microcontroladores con C',
+    place: 'Instituto Tecnologico de Lerma',
+    type: 'professional',
+    date: 'Diciembre de 2021',
+    description: 'Taller introductorio de programacion orientada a objetos con Processing',
+    current: false,
+  },
+  {
+    title: 'Programación de Arduino con Javascript',
+    place: 'Instituto Tecnologico de Lerma',
+    type: 'professional',
+    date: 'Diciembre de 2019',
+    description: 'Taller sobre el uso de Node y Johnny Five para programar Arduinos con Javascript',
+    current: false,
+  },
+  {
+    title: 'Aprendiendo de robotica con Iron-Man',
+    place: 'CreativeFest',
+    type: 'professional',
+    date: 'Mayo de 2019',
+    description: 'Taller sobre el uso de Node y Johnny Five para programar Arduinos con Javascript',
+    current: false,
+  },
+  {
+    title: 'Programación Básica con Arduino',
+    place: 'Instituto Tecnologico de Lerma',
+    type: 'professional',
+    date: 'Diciembre de 2016',
+    description: 'Taller de principios basicos de la programacion de placas Arduino UNO',
+    current: false,
+  },
+  {
+    title: 'Introducción a Processing',
+    place: 'Instituto Tecnologico de Lerma',
+    type: 'professional',
+    date: 'Junio de 2016',
+    description: 'Taller introductorio de programacion orientada a objetos con Processing',
+    current: false,
+  },
+]
+
+module.exports = workshopsData
