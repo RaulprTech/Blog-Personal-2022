13d3b6e6d21c967dea5ccdd8f83cbb5a17fd70c5
raulprtech <raul_vrm_2134@hotmail.com>
2022-12-28 22:31:28 -0600

    new Size screen

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -7,43 +7,45 @@ import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 
-const LayoutWrapper = ({ children, bgImage }) => {
+const LayoutWrapper = ({ children, bgImage, header = true }) => {
   return (
     <SectionContainer bgImage={bgImage}>
       <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
-                <div className="mr-3">
-                  <Logo />
-                </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
+        {header && (
+          <header className="flex items-center justify-between py-10">
+            <div>
+              <Link href="/" aria-label={siteMetadata.headerTitle}>
+                <div className="flex items-center justify-between">
+                  <div className="mr-3">
+                    <Logo />
                   </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
-                >
-                  {link.title}
-                </Link>
-              ))}
+                  {typeof siteMetadata.headerTitle === 'string' ? (
+                    <div className="hidden h-6 text-2xl font-semibold sm:block">
+                      {siteMetadata.headerTitle}
+                    </div>
+                  ) : (
+                    siteMetadata.headerTitle
+                  )}
+                </div>
+              </Link>
             </div>
-            <ThemeSwitch />
-            <MobileNav />
-          </div>
-        </header>
+            <div className="flex items-center text-base leading-5">
+              <div className="hidden sm:block">
+                {headerNavLinks.map((link) => (
+                  <Link
+                    key={link.title}
+                    href={link.href}
+                    className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                  >
+                    {link.title}
+                  </Link>
+                ))}
+              </div>
+              <ThemeSwitch />
+              <MobileNav />
+            </div>
+          </header>
+        )}
         <main className="mb-auto">{children}</main>
         <Footer />
       </div>
--- components/SectionContainer.js
+++ components/SectionContainer.js
@@ -6,7 +6,7 @@ export default function SectionContainer({ children, bgImage }) {
         backgroundImage: `url(${bgImage})`,
       }}
     >
-      <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+      <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-6xl xl:px-0">{children}</div>
     </section>
   )
 }
