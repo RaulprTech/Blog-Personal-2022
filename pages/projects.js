6693c65d8ef73d0025150b7a15fd39fbda12e19a
raulprtech <raul_vrm_2134@hotmail.com>
2022-12-28 22:33:11 -0600

    update projects

--- pages/projects.js
+++ pages/projects.js
@@ -21,7 +21,7 @@ export default function Projects() {
           </p>
         </div>
         <div className="container py-12">
-          <div className="-m-4 flex flex-wrap">
+          <div className="-m-4 flex flex-wrap justify-center">
             {projectsData.map((d) => (
               <Card
                 key={d.title}
